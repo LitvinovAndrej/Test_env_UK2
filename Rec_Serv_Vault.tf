06cda0a7064755b5287549a6d2a5ec0da65ddd67
Andrej Litvinov <andrej.litvinov@lrs.com>
2020-11-19 10:34:10 +0000

    fafafa

--- Rec_Serv_Vault.tf
+++ Rec_Serv_Vault.tf
@@ -18,7 +18,7 @@ resource "azurerm_recovery_services_vault" "rec_serv_vault1" {
   }
 }
 
-resource "azurerm_backup_policy_vm" "rec_serv_policy1" {
+resource "azurerm_backup_policy_vm" "recoverypolicy1" {
   name                           = "rec-serv-policy1"
   resource_group_name            = azurerm_resource_group.myterraformgroup.name
   recovery_vault_name            = azurerm_recovery_services_vault.rec_serv_vault1.name
@@ -27,47 +27,27 @@ resource "azurerm_backup_policy_vm" "rec_serv_policy1" {
 
   backup {
     frequency = "Weekly"
-    weekdays  = ["Saturday"]
-    time      = "09:00"
+    weekdays  = ["Friday"]
+    time      = "23:00"
   }
-  /*
-  retention_daily {
-    count = 10
-  }
-  */
+  
   retention_weekly {
-    count    = 2
-    weekdays = ["Sunday"]
-  }
-
-  /*
-  retention_monthly {
-    count    = 7
-    weekdays = ["Sunday", "Wednesday"]
-    weeks    = ["First", "Last"]
-  }
-
-  retention_yearly {
-    count    = 77
-    weekdays = ["Sunday"]
-    weeks    = ["Last"]
-    months   = ["January"]
-  }
-  */
+    count    = "5"
+    weekdays = ["Friday"]
+  } 
 }
 
 #ASSIGNING VMs TO RECOVERY SERVICE VAULT #ASSIGNING VMs TO RECOVERY SERVICE VAULT #ASSIGNING VMs TO RECOVERY SERVICE VAULT #ASSIGNING VMs TO RECOVERY SERVICE VAULT #ASSIGNING VMs TO RECOVERY SERVICE VAULT 
-/*
+
 resource "azurerm_backup_protected_vm" "vm1_r1" {
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   recovery_vault_name = azurerm_recovery_services_vault.rec_serv_vault1.name
   source_vm_id        = azurerm_virtual_machine.name1.id
-  backup_policy_id    = azurerm_backup_policy_vm.rec_serv_policy1.id
+  backup_policy_id    = azurerm_backup_policy_vm.recoverypolicy1.id
 }
 resource "azurerm_backup_protected_vm" "vm2_r1" {
   resource_group_name = azurerm_resource_group.myterraformgroup.name
   recovery_vault_name = azurerm_recovery_services_vault.rec_serv_vault1.name
   source_vm_id        = azurerm_virtual_machine.name2.id
-  backup_policy_id    = azurerm_backup_policy_vm.rec_serv_policy1.id
+  backup_policy_id    = azurerm_backup_policy_vm.recoverypolicy1.id
 }
-*/
